40ceff6388363c0952e8e726dc70fa4eb510e6cd
Max Pavlov <max@radacode.com>
2012-10-31 16:26:42 +0200

    Added general smart-site block, fixed logo.

---
+++ RadaCode.Web/RadaCode.Web/Controllers/WebDevelopmentController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RadaCode.Web.Controllers
+{
+    public class WebDevelopmentController : Controller
+    {
+        //
+        // GET: /WebDevelopment/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
